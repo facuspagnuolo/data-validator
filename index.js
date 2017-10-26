957ae7e9a350f11fa2949b140d49a0fed9aeb895
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-10-26 12:39:27 -0300

    Fix index require paths to use build versions

--- index.js
+++
@@ -1,7 +0,0 @@
-/*global exports*/
-
-const DataTypes = require('./src/DataTypes');
-const DataValidator = require('./src/DataValidator');
-
-exports.DataTypes = DataTypes;
-exports.DataValidator = DataValidator;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/*global exports*/
+
+const DataTypes = require('./DataTypes');
+const DataValidator = require('./DataValidator');
+
+exports.DataTypes = DataTypes;
+exports.DataValidator = DataValidator;
